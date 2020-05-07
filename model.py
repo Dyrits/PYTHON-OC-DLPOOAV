9f124188f3e1d4add73c9e5ab4297937f853077e
Dyrits <dyrits@outlook.fr>
2020-05-07 17:25:23 +0200

    A medthod has been partially added.

--- model.py
+++ model.py
@@ -40,6 +40,8 @@ class Zone:
     def __init__(self, left_bottom_corner, right_top_corner):
         self.lb_corner = left_bottom_corner
         self.rt_corner = right_top_corner
+        self.inhabitants = []
+        self.population = 0
 
     @classmethod
     def initialize_zones(cls):
@@ -48,6 +50,23 @@ class Zone:
                 zone = Zone(Position(latitude, longitude),
                             Position(latitude + 1, longitude + 1))
                 cls.ZONES.append(zone)
+                
+    @classmethod
+    def find_zone_of_position(cls, position):
+        #The zone is made of 64800 zones, wich can be divided in 180 rows and 360 columns.
+        # Longitude index:
+        longitude_index = int((position.longitude_degree - cls.MIN_LONGITUDE) / cls.DEGREE_WIDTH) # position.longitude_degree (-50) >> 130
+        # Number of columns per row:
+        longitude_degrees = int((cls.MAX_LONGITUDE - cls.MIN_LONGITUDE) / cls.DEGREE_WIDTH)  # 360
+        # Latitude row:
+        latitude_row = int((position.latitude_degrees - cls.MIN_LATITUDE) / cls.DEGREE_HEIGHT) # position.latitude_degree (5) >> 95
+        # There is {longitude_degrees} (360) index between two {latitude_row}.
+        # Latitude index:
+        latitude_index = latitude_row * longitude_degrees # position.latitude_degree (5) >> 95 * 360 = 34 200
+        index_zone = latitude_index + longitude_index  # position(5, -50) >> 34200 + 130 = 34330
+        
+        
+        
 
 
 def main():
@@ -59,6 +78,8 @@ def main():
             position = Position(latitude, longitude)
             agent = Agent(position, **agent_attributes)
             Zone.initialize_zones()
+            print(len(Zone.ZONES))
+        
 
 
 if __name__ == "__main__":
